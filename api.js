3210fe9a6eb1547ebccb90df6e8457bea173f37c
joaovitordev <jv17fc@gmail.com>
2021-06-18 20:36:38 -0300

    ajuste no get

--- api.js
+++ api.js
@@ -2,11 +2,10 @@ const API_KEY = 'zYFoNqN7GjP8AVazU73vLCDZX3WNXjieFbDiTBNP';
 const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2020-06-17&end_date=2020-06-17&api_key=${API_KEY}`;
 
 fetch(url)
-.then(response => response.json()) // retorna uma promise
-.then(result => {
-  result.map
-})
-.catch(err => {
-  // trata se alguma das promises falhar
-  console.error('Failed retrieving information', err);
-}); 
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.error('Failed retrieving information', err);
+    }); 
